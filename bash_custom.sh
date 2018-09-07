8fad8524c86328bea171f2108fd0b334df44169e
Yuri <ytitov@gmail.com>
2018-09-07 09:40:43 -0500

    added dropbox alias

--- bash_custom.sh
+++ bash_custom.sh
@@ -8,3 +8,6 @@ echo "Running bash_custom.sh"
 # ALIASES #########################################
 echo 'docker-sa will stop all running containers';
 alias docker-sa='docker stop $(docker ps -aq)'
+
+echo 'dropbox starts ~/.dropbox-dist/dropboxd';
+alias dropbox=~/.dropbox-dist/dropboxd; 
