cc44fd9c730d3ebee8d2cf9e85ec69decdf5f73f
Yuri <ytitov@gmail.com>
2018-09-17 10:36:47 -0500

    add example git log daterange command

--- bash_custom.sh
+++ bash_custom.sh
@@ -3,7 +3,7 @@
 #   . ~/.config/bash_custom.sh
 # fi
 
-echo "Running bash_custom.sh"
+echo "Running ~/.config/bash_custom.sh"
 
 # ALIASES #########################################
 echo 'docker-sa will stop all running containers';
@@ -11,3 +11,6 @@ alias docker-sa='docker stop $(docker ps -aq)'
 
 echo 'dropbox starts ~/.dropbox-dist/dropboxd';
 alias dropbox=~/.dropbox-dist/dropboxd; 
+
+echo 'git log --pretty=format:"%ad - %an: %s" --after="2018-09-09" --until="2018-09-15"'
+echo 'gives you a date range'
