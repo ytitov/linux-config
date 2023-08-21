25eb34008b8db72ee9ecccda53399fb99ca7d328
Yuri Titov <ytitov@gmail.com>
2023-08-21 05:53:12 -0500

    add docker utils

--- bash-setup.docker-utils.sh
+++ bash-setup.docker-utils.sh
@@ -2,3 +2,7 @@
 show_line_with_title "Docker utilities - bash-setup.docker-utils.sh"
 msg_info docker_clean_images to remove dangling images
 alias docker_clean_images='docker rmi $(docker images -a --filter=dangling=true -q)'
+msg_info "show dangling images" 'docker rmi $(docker images -a --filter=dangling=true -q)'
+msg_info docker_stop_all "to stop all containers"
+alias docker_stop_all='docker stop $(docker ps -q)'
+msg_info "clear all cache" 'docker builder prune'
